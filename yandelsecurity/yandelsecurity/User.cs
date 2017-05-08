991c28fedc856e4d2931a1b66be11aaaf0a3df0d
Emi <emontesdeoc@gmail.com>
2017-05-08 13:54:39 +0100

    Yandel security test

---
+++ yandelsecurity/yandelsecurity/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yandelsecurity
+{
+    public class User
+    {
+
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string UserKey { get; set; }
+        public string PasswordKey { get; set; }
+        public string EmailKey { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
---
+++ yandelsecurity/yandelsecurity/Yandel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yandelsecurity
+{
+    public class Yandel
+    {
+        public string UserKey { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+
+
+
+        public string EncodeString(string UserKey, string String)
+        {
+            string res = "";
+
+            string nonencodedstring = String;
+            string encodedstring = Convert.ToBase64String(Encoding.UTF8.GetBytes(nonencodedstring));
+
+            string encodedconcat = UserKey + encodedstring;
+            string reencodedconcat = Convert.ToBase64String(Encoding.UTF8.GetBytes(encodedconcat));
+
+            Random rnd = new Random();
+
+            int i = rnd.Next(1, 4);
+
+            //string[] splitEncoded = reencodedconcat.Split()
+
+
+            return res;
+        }
+
+    }
+}
